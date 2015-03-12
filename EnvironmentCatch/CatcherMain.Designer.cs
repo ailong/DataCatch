775dc3461e247813e8822bc7bb81a37b45db7946
chenlong <roen88@163.com>
2015-03-12 15:16:33 +0800

    Prepare of VisialStudio

---
+++ EnvironmentCatch/CatcherMain.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace EnvironmentCatch
+{
+    partial class SuperCatcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SuperCatcher));
+            this.SuspendLayout();
+            // 
+            // SuperCatcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 655);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SuperCatcher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SuperCatcher-Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
